a5dd5b43910ded744f4e082965c63bc6844fb9f0
Pau Novella <pnovella@neutrinos1.local>
2015-01-29 15:16:04 +0100

    Environmental variables added

---
+++ emodel/Environment.h
@@ -0,0 +1,34 @@
+#ifndef __Environment_H__
+#define __Environment_H__
+
+/**
+ * @file Environment.h
+ * 
+ * @brief Useful typedef and enums
+ *    
+ * @author Pau Novella  <pau.novella@ific.uv.es>
+ *
+ * @version Revision 1.0.0
+ *
+ * @date January 2015
+ *
+ */
+
+
+namespace gate{  
+  
+  //! not defined number
+  static const double nan = -9999.9999;
+
+  //! Data type: MC-TRUTH, MC-DIGI, or DATA 
+  enum DATATYPE{ NODTYPE, MCTRUTH,MC,DATA};
+  
+  //! hit state: raw, recosntructed or calibrated
+  enum STATE{ NOSTATE, RAW,RECOED,CALIB};
+  
+  //! sensor type: SiPM or PMT
+  enum SENSORTYPE{ NOSTYPE,SIPM,PMT};
+
+}; // end of gate namespace
+
+#endif
