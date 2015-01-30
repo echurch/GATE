203d57b3d239c60ab7e88b1659331c9d1a6cd3c5
pnovella <pau.novella@ific.uv.es>
2015-01-30 15:00:06 +0100

    link vector of hits, so it is an iterable when exposed to python

--- emodel/LinkDef.hh
+++ emodel/LinkDef.hh
@@ -13,12 +13,13 @@
 #pragma link C++ class gate::Hit+;
 #pragma link C++ class gate::Point3D+;
 
+#pragma link C++ class std::vector<gate::Hit*>;
 #pragma link C++ class std::pair<gate::SENSORTYPE,gate::Hit*>;
 
 
-// why the hell does CINT need this dictionaries?????????
-#pragma link C++ class std::pair<std::string,std::string>;
-#pragma link C++ class std::pair<std::string,TObject*>;
+// why the hell does CINT need this dictionaries for ParamStore?????????
+//#pragma link C++ class std::pair<std::string,std::string>;
+//#pragma link C++ class std::pair<std::string,TObject*>;
 //#pragma link C++ class std::pair<std::string, vector<double> >;
 //#pragma link C++ class std::pair<std::string, vector<int> >;
 //#pragma link C++ class std::pair<std::string, vector<TObject*> >;
