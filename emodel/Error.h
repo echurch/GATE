0c27788426d4585efb93a673a85872bd2bcb7a16
pnovella <pau.novella@ific.uv.es>
2015-03-04 12:19:22 +0100

    Doxygen doumentation improved

--- emodel/Error.h
+++ emodel/Error.h
@@ -1,23 +1,3 @@
-/* -*- mode: c++ -*- */
-//! error.h
-/** This header file contains a simplified version
- *  of error control
- */
-/*   
- * 
- * Copyright (C) 2004 J.J. Gomez-Cadenas
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
- * your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- */
-
 #ifndef __BHERROR__
 #define __BHERROR__
 
@@ -25,6 +5,20 @@
 #include <exception>
 #include <stdexcept>
 #include <string>  
+
+/**
+ * @file Error.h
+ * 
+ * @brief  simplified version of error control
+ *   
+ * @author J.J. Gomez-Cadenas, Pau Novella  <pau.novella@ific.uv.es>
+ *
+ * @version Revision 1.0.0
+ *
+ * @date January 2015
+ *
+ * @ingroup emodel
+ */
 
 //!Global namespace for the library
 namespace gate{
