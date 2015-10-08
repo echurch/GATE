070dce996a09e0364e94115300498cd35ed08183
pnovella <pau.novella@ific.uv.es>
2015-10-08 18:21:47 +0200

    minor improvement in Info

--- emodel/Track.cc
+++ emodel/Track.cc
@@ -10,13 +10,16 @@ gate::Track::Track() : BTrack(){}
 void gate::Track::Info(std::ostream& s) const{
 //=======================================================
 
-    s << "Track instance: "<<std::endl;
+    s << "============ Track instance: =========="<<std::endl;
 
     BTrack::Info(s);
     
     s << "Sensor Type:  "<<_sType<<std::endl;
     
     s << "track State:  "<<_state<<std::endl;
+
+    s << "======================================="<<std::endl;
+
 }
 
 //=======================================================
