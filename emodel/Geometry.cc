bb99b5e5a92eb5db228140e4b7d7cb7e5ee9a9dc
pau Novella <pau.novella@ific.uv.es>
2017-01-16 17:01:03 +0100

    key of sensor map is now SensorID

--- emodel/Geometry.cc
+++ emodel/Geometry.cc
@@ -24,7 +24,7 @@ gate::Geometry::~Geometry(){
 void gate::Geometry::AddSensor(gate::Sensor* sens){
 //=======================================================
     
-    _sensors[sens->GetID()]=sens;
+    _sensors[sens->GetSensorID()]=sens;
 }
 
 //=======================================================
